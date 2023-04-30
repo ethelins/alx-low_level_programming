6ee109b26f500bdc491c6cf41c759a349c0b7b3b
Ethel Mpi <ethelins>
2023-04-30 16:57:37 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,22 @@
-1  #include "list.h"
-2
-3  /**
-4   *  print_listint - prints all the elements of a linked list
-5   *  @h: head of the list
-6   *
-7   *  Return: the number of nodes
-8   */
-9  size_t print_listint(const listint_t *h) 
-10 {
-11	  const listint_t *cursor = h;
-12	  size_t count = 0;
-13
-14	  while (cursor != NULL)
-15        {		  
-16		 printf("%d\n", cursor->n)
-17               count += 1;
-18		 cursor = cursor->next;
-19	  }
-20	  return (count);
-21 }
+#include "list.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h) 
+{
+       const listint_t *cursor = h;
+       size_t count = 0;
+
+       while (cursor != NULL)
+       {		  
+	       printf("%d\n", cursor->n)
+               count += 1;
+	       cursor = cursor->next;
+       }
+       return (count);
+}       
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,51 +1,50 @@
-1   #ifndef _LISTS_
-2   #define _LISTS_
-3
-4   #include <stdio.h>
-5   #include <stdlib.h>
-6
-7   /**
-8    * strut listint_s -singly linked list
-9    * @n: integer
-10   * @next: points to the next node
-11   *
-12   * Description: singly linked list node structure
-13   * for Holberton project
-14   */
-15  typedef strct listint_s
-16  {
-17	    int n;
-18     	    struct listint_s *next;
-19  } listint_t;
-20
-21  /**
-22   * struct listp_s - singly linked list
-23   * @p: pointers of nodes
-24   * @next: points to the next node
-25   *
-26   * Description: singly linked list of poiters
-27   */
-28  typedef struct listp_s
-29  {
-30  	    void *p;
-31	    struct listp_s *next;
-32  } listp_t;
-33
-34  size_t print_listint(const listint_t *h);
-35  size-t listint_len(const listint_t *h);
-36  listint_t *add_nodeint(listint_t **head, const int n);
-37  listint_t *add_nodeint_end(listint_t **head, const int n);
-38  void free_listint(listint_t *head);
-39  void free_listint2(listint_t **head);
-40  int pop_listint(listint_t **head);
-41  listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-42  int sum_listint(listint_t *head);
-43  listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx,int n);
-44  int delete_nodeint_at_index(listint_t **head,unsigned int index);
-45  listint_t *reverse_listint(listint_t **head);
-46  size_t print_listint_safe(const listint_t *head);
-47  size_t free_listint_safe(listint_t **h);
-48  listint_t *find_listint_loop(listint_t *head);
-49
-50  #endif
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t
+
+/**
+ * struct listp_s - singly linked list
+ * @p: poiters of nodes
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	stuct listp_s *next;
+} listp_t;
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int idex);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head,unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+
+#endif
 

fbcfe34eafe945bda816fc53accec4316b781391
Ethel Mpi <ethelins>
2023-05-01 09:20:19 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,7 +6,7 @@
  *
  * Return: the number of nodes
  */
-size_t print_listint(const listint_t *h) 
+size_t print_listint(const listint_t *h)
 {
 	const listint_t *cursor = h;
 	size_t count = 0;
@@ -14,14 +14,8 @@ size_t print_listint(const listint_t *h)
 	while (cursor != NULL)
 	{
 		printf("%d\n", cursor->n);
-
 		count += 1;
-
 		cursor = cursor->next;
 	}
 	return (count);
 }
-
-       
-
-

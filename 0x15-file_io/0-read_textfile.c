c4749698369a123313bdac2359efef8d4c25eca1
Ethel Mpi <ethelins>
2023-05-08 16:38:42 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,44 @@
-#include <unistd.h>
-#include <fcntl.h>
+/*
+ * File: 0-read_textfile.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * read_textfile - prints text from a file
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * @filename: name of the file
- * @letters: number of characters to read
- *
- * Return: actual number of letters read, 0 if end of file
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file;
-	int length, wrotechars;
-	char *buf;
+	ssize_t o, r, w;
+	char *buffer;
 
-	if (filename == NULL || letters == 0)
-		return (0);
-	buf = malloc(sizeof(char) * (letters));
-	if (buf == NULL)
+	if (filename == NULL)
 		return (0);
 
-	file = open(filename, O_RDONLY);
-	if (file == -1)
-	{
-		free(buf);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
-	}
-	length = read(file, buf, letters);
-	if (length == -1)
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buf);
-		close(file);
+		free(buffer);
 		return (0);
 	}
 
-	wrotechars = write(STDOUT_FILENO, buf, length);
+	free(buffer);
+	close(o);
 
-	free(buf);
-	close(file);
-	if (wrotechars != length)
-		return (0);
-	return (length);
+	return (w);
 }
